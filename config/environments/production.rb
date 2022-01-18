4d20e86fab53c95ba93fea010fb449533045acbb
Brandon Gannicott <brandon.gannicott@gmail.com>
2022-01-17 23:09:43 -0600

    clean up

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,6 +70,18 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: "www.skypiratedb.herokuapp.com" }
 
+  config.action_mailer.delivery_method = :smtp
+
+  # SES
+  config.action_mailer.smtp_settings = {
+    address: "email-smtp.us-east-2.amazonaws.com",
+    port: 587,
+    user_name: ENV["SES_USERNAME"],
+    password: ENV["SES_PASSWORD"],
+    authentication: "plain",
+    enable_starttls_auto: true,
+  }
+
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
